5acab34cb99c3cedf8e23f63d9c0be25a00eb1c8
ElisaBertuccioli <81807226+ElisaBertuccioli@users.noreply.github.com>
2021-06-17 15:48:29 +0200

    Add files via upload
    Vérification des champs d'un formulaire

---
+++ TP2.js
@@ -0,0 +1,29 @@
+﻿$('#pseudo').keyup(function(){
+  var pseudo = $(this).val();
+  var longueur_pseudo = pseudo.length;
+  if ((longueur_pseudo >= 2) && (longueur_pseudo <= 15)){
+    $(this).css('border-color', 'blue');
+  }else{
+    $(this).css('border-color', 'red');
+  }
+});
+
+$('#mail').keyup(function(){
+  var mail = $(this).val();
+  if (mail.includes('@')){
+     $(this).css('border-color', 'blue');
+  }else{
+    $(this).css('border-color', 'red');
+  }
+
+});
+
+$('#password').keyup(function(){
+  var password = $(this).val();
+  var longueur_password = password.length;
+  if ((/\d/.test(password)) && (longueur_password >=6)){
+    $(this).css('border-color', 'blue');
+  }else{
+    $(this).css('border-color', 'red');
+  }
+})
